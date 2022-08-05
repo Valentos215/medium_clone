4ca136f8cf23eb9836e9f97625e84200702e0d6e
valentin_215 <teskavale46@gmail.com>
2022-08-05 17:45:50 +0300

    Deleting post

--- src/pages/article/Article.js
+++ src/pages/article/Article.js
@@ -1,49 +1,106 @@
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import ErrorMessage from "../../components/ErrorMessage";
 import Loading from "../../components/Loading";
 import TagList from "../../components/TagList";
+import { CurrentUserContext } from "../../contexts/currentUser";
 import useFetch from "../../hooks/useFetch";
 
 const Article = (props) => {
   const slug = props.match.params.slug;
   const apiUrl = `/articles/${slug}`;
-  const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+  const [
+    {
+      response: fetchingResponse,
+      isLoading: fetchingInProcess,
+      error: fetchingError,
+    },
+    doFetch,
+  ] = useFetch(apiUrl);
+  const [{ response: deletingResponse }, doDelete] = useFetch(apiUrl);
+  const [currentUserState] = useContext(CurrentUserContext);
+  const [isSuccessfullDelete, setIsSuccessfullDelete] = useState(false);
+
+  const isAuthor = () => {
+    if (!fetchingResponse || !currentUserState.isLoggedIn) {
+      return false;
+    }
+    return (
+      fetchingResponse.article.author.username ===
+      currentUserState.currentUser.username
+    );
+  };
+
+  const deleteArticle = () => {
+    doDelete({
+      method: "delete",
+    });
+  };
 
   useEffect(() => {
     doFetch();
   }, [doFetch]);
 
+  useEffect(() => {
+    if (!deletingResponse) return;
+    setIsSuccessfullDelete(true);
+  }, [deletingResponse]);
+
+  if (isSuccessfullDelete) return <Redirect to="/" />;
   return (
     <div className="article-page">
       <div className="banner">
-        {!isLoading && response && (
+        {!fetchingInProcess && fetchingResponse && (
           <div className="container">
-            <h1>{response.article.title}</h1>
+            <h1>{fetchingResponse.article.title}</h1>
             <div className="article-meta">
-              <Link to={`/profiles/${response.article.author.username}`}>
-                <img src={response.article.author.image} alt="" />
+              <Link
+                to={`/profiles/${fetchingResponse.article.author.username}`}
+              >
+                <img src={fetchingResponse.article.author.image} alt="" />
               </Link>
               <div className="info">
-                <Link to={`/profiles/${response.article.author.username}`}>
-                  {response.article.author.username}
+                <Link
+                  to={`/profiles/${fetchingResponse.article.author.username}`}
+                >
+                  {fetchingResponse.article.author.username}
                 </Link>
-                <span className="date">{response.article.createdAt}</span>
+                <span className="date">
+                  {fetchingResponse.article.createdAt}
+                </span>
               </div>
+              {isAuthor() && (
+                <span>
+                  <Link
+                    className="btn btn-outline-secondary btn-sm"
+                    to={`/articles/${fetchingResponse.article.slug}/edit`}
+                  >
+                    <i className="ion-edit"></i>
+                    Edit article
+                  </Link>
+                  <button
+                    className="btn btn-outline-danger btn-sm"
+                    onClick={deleteArticle}
+                  >
+                    <i className="ion-trash-a"></i>
+                    Delete article
+                  </button>
+                </span>
+              )}
             </div>
           </div>
         )}
       </div>
       <div className="container page">
-        {isLoading && <Loading />}
-        {error && <ErrorMessage />}
-        {!isLoading && response && (
+        {fetchingInProcess && <Loading />}
+        {fetchingError && <ErrorMessage />}
+        {!fetchingInProcess && fetchingResponse && (
           <div className="row article-content">
             <div className="col-xs-12">
               <div>
-                <p>{response.article.body}</p>
+                <p>{fetchingResponse.article.body}</p>
               </div>
-              <TagList tags={response.article.tagList} />
+              <TagList tags={fetchingResponse.article.tagList} />
             </div>
           </div>
         )}
